ec4989da027478b4bcb9eaaeebfb6cab1329f32a
h-inouchi <hibird2720804@gmail.com>
2020-05-30 12:50:09 +0900

    books index fix

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,7 +5,11 @@
   <div id="shelf" class="maple shelf-front-area" data-display="front" data-template="maple">
       <div class="shelf-wrapper">
         <div class="shelf-area">
-
+@foreach($books as $book)
+          <div class="book">
+            {{ $book->title }}
+          </div>
+@endforeach
         </div>
       </div>
   </div>
